5f2513528a35918bccfb2481e8e10ee090918225
Marcus <88283829+PyMarcus@users.noreply.github.com>
2022-07-01 20:02:05 -0300

    Add files via upload

--- src/Main.py
+++ src/Main.py
@@ -5,17 +5,27 @@ from Server import Server
 
 if __name__ == '__main__':
     """
-    Por padrão, está implementado o ip 0.0.0.0
-    e a porta 9000
+    Por padrão, está implementado o ip localhost
+    e a porta 30000
     """
     try:
-        if sys.argv[1]:
-            if sys.argv[1] == "Client":
-                client = Client("0.0.0.0", 9000)
-                client.connect()
-            elif sys.argv[1] == "Server":
-                server = Server("0.0.0.0", 9000)
-                server.connectToClient()
+        print(sys.argv)
+        if sys.argv[0]:
+            if sys.argv[1]:
+                if sys.argv[1] == "Client":
+                    client = Client("localhost", 30000)
+                    client.connect()
+                elif sys.argv[1] == "Server":
+                    server = Server("localhost", 30000)
+                    server.connectToClient()
+            else:
+                if sys.argv[1] == "Client":
+                    client = Client(sys.argv[2], int(sys.argv[3]))
+                    client.connect()
+                elif sys.argv[1] == "Server":
+                    client = Server(sys.argv[2], int(sys.argv[3]))
+                    client.connectToClient()
     except IndexError:
         print("[*] Para executar, abra o terminal e digite:")
-        print("[*] python Main.py Client ou Server, lembrando que o servidor deve ser iniciado primeiro")
+        print("[*] python Main.py Client ou Server ip porta [ip e porta sao opcionais], lembrando que o servidor deve "
+              "ser iniciado primeiro")
